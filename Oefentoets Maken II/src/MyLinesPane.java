7cef43da5114e80f4736532f9a750e8cd18106b2
RedmarAlkema <rag.alkema@student.avans.nl>
2023-06-02 00:55:19 +0200

    Main, MyLinesPane & MyScene bewerkt
    - Elke class constructor gegeven
    - methodes toegevoegd
    	Main:
    		-start
    	MyLinesPane:
    		- doIt
    		- showLines
    	MyScene:
    		- setFirst

--- Oefentoets Maken II/src/MyLinesPane.java
+++ Oefentoets Maken II/src/MyLinesPane.java
@@ -1,4 +1,44 @@
+import java.util.ArrayList;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 
-public class MyLinesPane {
+public class MyLinesPane extends MyAbstractPane {
+
+	// constants
+	private double margin = 10;
+
+	// instance variabeles
+	private ArrayList<Line> lines;
+	private Pane centerpane;
+	private Color linecolor;
+
+	// constructor
+	public MyLinesPane(Color backgroundcolor, Color linec, MyScene scene) {
+		super(backgroundcolor, scene);
+		super.setTextOnButton("line");
+		linecolor = linec;
+		lines = new ArrayList<Line>();
+		centerpane = new Pane();
+		setCenter(centerpane);
+	}
+
+	@Override
+	public void doIt() {
+		if (lines.size() < 26) {
+			double n = lines.size() + 1;
+			double linewidth = this.getWidth() - margin;
+			Line newline = new Line(margin, margin * n, linewidth, margin * n);
+			newline.setStrokeWidth(4);
+			newline.setStroke(linecolor);
+			lines.add(newline);
+			showLines();
+		}
+	}
+
+	private void showLines() {
+		centerpane.getChildren().clear();
+		centerpane.getChildren().addAll(lines);
+	}
 
 }
--- Oefentoets Maken II/src/MyScene.java
+++ Oefentoets Maken II/src/MyScene.java
@@ -1,4 +1,28 @@
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
-public class MyScene {
+
+public class MyScene extends Scene {
+
+	// instance variables
+	private Pane root;
+
+	// constructor
+	public MyScene() {
+		super(new Pane());
+		root = new HBox();
+		root.getChildren().add(new MyLinesPane(Color.ORANGE, Color.BLACK, this));
+		root.getChildren().add(new MyLinesPane(Color.DARKGREEN, Color.WHITE, this));
+		root.getChildren().add(new MyLettersPane(Color.LIGHTBLUE, Color.BLACK, this));
+		root.getChildren().add(new MyLettersPane(Color.PURPLE, Color.WHITE, this));
+		this.setRoot(root);
+	}
+
+	public void setFirst(MyAbstractPane current) {
+		root.getChildren().remove(current);
+		root.getChildren().add(0, current);
+	}
 
 }
