9520b39f2eaada755d11af4e387ba7ea28c61b14
RedmarAlkema <rag.alkema@student.avans.nl>
2023-06-02 01:02:37 +0200

    MyAbstractPane, MyButtonPane & MyLettersPane bewerkt
    - Constructor aan elke class toegevoegd
    - Methodes toegevoegd
    	MyAbstractPane:
    	- setFirst
    	- setTextOnButton
    	- doIt
    	MyButtonPane:
    	- setTextOnButton
    	- handleDo
    	- handleFirst
    	MyLettersPane:
    	- doIt
    	- showLetters

--- Oefentoets Maken II/src/MyLettersPane.java
+++ Oefentoets Maken II/src/MyLettersPane.java
@@ -1,4 +1,59 @@
+import java.util.ArrayList;
 
-public class MyLettersPane {
+import javafx.geometry.Insets;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Shape;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+
+
+public class MyLettersPane extends MyAbstractPane {
+
+	// constants
+	private final double margin = 10;
+	final private Font myfont = Font.font("arial", FontWeight.BOLD, 30);
+
+	// instance variabeles
+	private ArrayList<Shape> letters;
+	private Pane centerpane;
+	private Color linecolor;
+
+	// constructor
+	public MyLettersPane(Color backgroundcolor, Color linec, MyScene scene) {
+		super(backgroundcolor, scene);
+		super.setTextOnButton("letter");
+		linecolor = linec;
+		letters = new ArrayList<Shape>();
+		centerpane = new FlowPane(5, 5);
+		centerpane.setPadding(new Insets(margin));
+		setCenter(centerpane);
+	}
+
+	@Override
+	public void doIt() {
+		if (letters.size() < 26) {
+			char c = (char) ('A' + letters.size());
+			Text letter = new Text("" + c);
+			letter.setFont(myfont);
+			letter.setStrokeWidth(2);
+			if (linecolor == Color.BLACK) {
+				letter.setFill(Color.WHITE);
+			}
+			else {
+				letter.setFill(Color.BLACK);
+			}
+			letter.setStroke(linecolor);
+			letters.add(letter);
+			showLetters();
+		}
+	}
+
+	private void showLetters() {
+		centerpane.getChildren().clear();
+		centerpane.getChildren().addAll(letters);
+	}
 
 }
